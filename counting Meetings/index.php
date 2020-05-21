c64fcb6bb77d6b7d3c236f235352598e9073415b
Alex castro <alexoliveira7x@gmail.com>
2020-05-21 12:14:30 -0300

    trying

--- counting Meetings/index.php
+++ counting Meetings/index.php
@@ -9,31 +9,29 @@
  *  2. INTEGER_ARRAY lastDay
  */
 
- function fillNumbers($start, $end) {
-    if($start === $end) {
-        return [
-            0 => $start,
-            1 => $end
+function countMeetings($firstDay, $lastDay) {
+    foreach ($lastDay as $key => $item) {
+        $days[] = [
+            0 => $firstDay[$key],
+            1 => $lastDay[$key]
         ];
     }
+    rsort($days);
 
-    $fills = [];
-    for ($i= $start; $start < $end; $i++) { 
-        array_push($fills, $start);
-    }
-    
-    return $fills;
- }
+    $days = array_map(function($item) {
+        $day = range($item[0], $item[1]);
+        $day = array_filter($day, function ($row) use ($item) {
+            if(array_key_exists($row, $item)) {
+                return $row;
+            }
+        });
 
-function countMeetings($firstDay, $lastDay) {
-    foreach($lastDay as $key => $item) {
-        $days[] = fillNumbers($firstDay[$key],$lastDay[$key]);
-    }
-
-    var_dump($days);
+        return $day;
+    }, $days);
+    return count($days);
 }
 
-$fisrt_day = [1, 2, 1, 2, 20];
-$last_day = [3, 2, 1, 3, 22];
+$fisrt_day = [1,1,2];
+$last_day = [1,2,3];
 
- countMeetings($fisrt_day, $last_day);
+var_dump(countMeetings($fisrt_day, $last_day));
