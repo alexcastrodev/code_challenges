e3d59b59e477eb44356490dfbd80cd321ed4f912
Alex Castro <alexoliveira7x@gmail.com>
2020-05-21 23:04:16 -0300

    working, but.. it's getting overflow in 5 tests

--- counting Meetings/index.php
+++ counting Meetings/index.php
@@ -8,23 +8,19 @@ function countMeetings($firstDay, $lastDay) {
         ];
     }
 
-   $days  = array_map(function($item) {
-        return range($item[0], $item[1]);
-    }, $days);
-
+    $meetings = [];
     rsort($days);
 
-    $meetings = [];
-    array_map(function($item) use (&$meetings) {
-        $item[1] = count($item) > 1 ? $item[1] : $item[0];
-       if($item[0] == $item[1]) {
-            $meetings[] = $item[0];
-       } else {
-        $meetings[] = in_array($item[0], $meetings) ?  $item[0] : $item[1];
-       }
-    },$days);
-    // var_dump($meetings);
-    $meetings  = array_unique($meetings);
+        foreach($days as $key => $item) {
+                $avalaible = range($item[0], $item[1]);
+                foreach($avalaible as $day) {
+                        if(!in_array($day, $meetings)) {
+                                $meetings[] =  $day;
+                        break;
+                        }
+                } 
+        }
+
     return count($meetings);
 }
 
@@ -37,10 +33,10 @@ function countMeetings($firstDay, $lastDay) {
 
 /*
         @expect integer  3
-        @got integer 2
+        @got integer 3
 */
-$fisrt_day = [1, 2, 1, 2, 2];
-$last_day = [3, 2, 1, 3, 3];
+// $fisrt_day = [1, 2, 1, 2, 2];
+// $last_day = [3, 2, 1, 3, 3];
 
 
 /*
@@ -49,7 +45,5 @@ $last_day = [3, 2, 1, 3, 3];
 */
 // $fisrt_day = [1, 10, 11];
 // $last_day = [11, 10, 11];
-
-
 
 echo countMeetings($fisrt_day, $last_day);
