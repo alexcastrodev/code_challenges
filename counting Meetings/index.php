943c877801b46be688b88945d231a9353d5435d2
Alex Castro <alexoliveira7x@gmail.com>
2020-05-21 22:08:12 -0300

    upgrading and tryings

--- counting Meetings/index.php
+++ counting Meetings/index.php
@@ -1,37 +1,55 @@
 <?php
 
-/*
- * Complete the 'countMeetings' function below.
- *
- * The function is expected to return an INTEGER.
- * The function accepts following parameters:
- *  1. INTEGER_ARRAY firstDay
- *  2. INTEGER_ARRAY lastDay
- */
-
 function countMeetings($firstDay, $lastDay) {
     foreach ($lastDay as $key => $item) {
         $days[] = [
-            0 => $firstDay[$key],
-            1 => $lastDay[$key]
+           0 => $firstDay[$key],
+           1 => $lastDay[$key]
         ];
     }
+
+   $days  = array_map(function($item) {
+        return range($item[0], $item[1]);
+    }, $days);
+
     rsort($days);
 
-    $days = array_map(function($item) {
-        $day = range($item[0], $item[1]);
-        $day = array_filter($day, function ($row) use ($item) {
-            if(array_key_exists($row, $item)) {
-                return $row;
-            }
-        });
-
-        return $day;
-    }, $days);
-    return count($days);
+    $meetings = [];
+    array_map(function($item) use (&$meetings) {
+        $item[1] = count($item) > 1 ? $item[1] : $item[0];
+       if($item[0] == $item[1]) {
+            $meetings[] = $item[0];
+       } else {
+        $meetings[] = in_array($item[0], $meetings) ?  $item[0] : $item[1];
+       }
+    },$days);
+    // var_dump($meetings);
+    $meetings  = array_unique($meetings);
+    return count($meetings);
 }
 
-$fisrt_day = [1,1,2];
-$last_day = [1,2,3];
+/*
+        @expect integer  2, 
+        @got integer 2  
+*/
+// $fisrt_day = [1,1,2];
+// $last_day = [1,2,2];
 
-var_dump(countMeetings($fisrt_day, $last_day));
+/*
+        @expect integer  3
+        @got integer 2
+*/
+$fisrt_day = [1, 2, 1, 2, 2];
+$last_day = [3, 2, 1, 3, 3];
+
+
+/*
+        @expect integer  3
+        @got integer 3
+*/
+// $fisrt_day = [1, 10, 11];
+// $last_day = [11, 10, 11];
+
+
+
+echo countMeetings($fisrt_day, $last_day);
